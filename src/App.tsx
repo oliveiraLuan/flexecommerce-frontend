3a10891e5fb52f0065fd76591fc9a9cf50099b43
Luan de Oliveira <luan012012@hotmail.com>
2023-11-02 22:05:35 -0300

    Componentes Produto, Categoria, Buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import './App.css'
-import pneu from './assets/pneu.png';
+import ButtonPrimary from './components/ButtonPrimary';
+import ButtonSecondary from './components/ButtonSecondary';
 import HeaderClient from './components/HeaderClient';
-
+import ProductDetailsCard from './components/ProductDetailsCard';
 
 function App() {
 
@@ -10,35 +11,10 @@ function App() {
     <HeaderClient/>
     <main>
       <section id="product-details-section" className="dsc-container">
-        <div className="dsc-card dsc-mb20">
-          <div className="dsc-product-details-top dsc-line-bottom">
-            <img src={pneu} alt="Pneu Continental" />
-          </div>
-          <div className="dsc-product-details-bottom">
-            <h3>R$ 280,00</h3>
-            <h4>Pneu Continental 185/65 r15</h4>
-            <p>
-              Aumente a quilometragem e melhore o desempenho com o Pneu Conti 185/65 R15 ContiPowerContact 2 88H. Desenvolvido com tecnologia avançada, este pneu oferece durabilidade excepcional e desempenho superior em todas as condições de estrada.
-              Com uma vida útil estendida, o ContiPowerContact 2 proporciona até 20% a mais de quilometragem em comparação com outras marcas . Além disso, a tecnologia Eco Plus+ integrada garante uma economia de combustível , tornando-o uma escolha econômica e sustentável para seu veículo.
-              Desfrute de uma condução suave e silenciosa com a tecnologia Conti Noise Cancelling, que reduz o ruído do atrito entre o pneu e a estrada. A tecnologia Conti Safety System melhora a aderência e o controle do veículo, proporcionando uma direção confiável em diferentes condições de estrada.
-            </p>
-            <div className="dsc-category-container">
-              <div className="dsc-category">
-                Pneus
-              </div>
-              <div className="dsc-category">
-                Carros
-              </div>
-            </div>
-          </div>
-        </div>
+        <ProductDetailsCard/>
         <div className="dsc-btn-page-container">
-          <div className="dsc-btn dsc-btn-blue">
-            Comprar
-          </div>
-          <div className="dsc-btn dsc-btn-white">
-            Início
-          </div>
+            <ButtonPrimary/>
+            <ButtonSecondary/>
         </div>
       </section>
     </main>
---
+++ src/components/ProductDetailsCard/index.tsx
@@ -0,0 +1,26 @@
+import pneu from '../../assets/pneu.png';
+import ProductCategory from '../ProductCategory';
+import './styles.css';
+
+export default function ProductDetailsCard(){
+    return (
+        <div className="dsc-card dsc-mb20">
+          <div className="dsc-product-details-top dsc-line-bottom">
+            <img src={pneu} alt="Pneu Continental" />
+          </div>
+          <div className="dsc-product-details-bottom">
+            <h3>R$ 280,00</h3>
+            <h4>Pneu Continental 185/65 r15</h4>
+            <p>
+              Aumente a quilometragem e melhore o desempenho com o Pneu Conti 185/65 R15 ContiPowerContact 2 88H. Desenvolvido com tecnologia avançada, este pneu oferece durabilidade excepcional e desempenho superior em todas as condições de estrada.
+              Com uma vida útil estendida, o ContiPowerContact 2 proporciona até 20% a mais de quilometragem em comparação com outras marcas . Além disso, a tecnologia Eco Plus+ integrada garante uma economia de combustível , tornando-o uma escolha econômica e sustentável para seu veículo.
+              Desfrute de uma condução suave e silenciosa com a tecnologia Conti Noise Cancelling, que reduz o ruído do atrito entre o pneu e a estrada. A tecnologia Conti Safety System melhora a aderência e o controle do veículo, proporcionando uma direção confiável em diferentes condições de estrada.
+            </p>
+            <div className="dsc-category-container">
+              <ProductCategory/>
+              <ProductCategory/>
+            </div>
+          </div>
+        </div>
+    );
+}
