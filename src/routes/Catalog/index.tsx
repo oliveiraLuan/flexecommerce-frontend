79656859bfc3d1e1fecc57dc9ca600210ce3ad0e
Luan de Oliveira <luan012012@hotmail.com>
2023-11-03 21:50:57 -0300

    Criado componente para card do catálogo

--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -1,7 +1,7 @@
 import HeaderClient from "../../components/HeaderClient"
-import pneuImg from "../../assets/pneu.png";
 import './styles.css';
 import SearchBar from "../../components/SearchBar";
+import CatalogCard from "../../components/CatalogCard";
 
 export default function Catalog() {
   return (
@@ -12,98 +12,14 @@ export default function Catalog() {
           <SearchBar/>
 
           <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
-            <div className="dsc-card">
-              <div className="dsc-catalog-card-top dsc-line-bottom">
-                <img src={pneuImg} alt="Computer" />
-              </div>
-              <div className="dsc-catalog-card-bottom">
-                <h3>R$ 320,00</h3>
-                <h4>Pneu Continental 185/65 r15</h4>
-              </div>
-            </div>
-            <div className="dsc-card">
-              <div className="dsc-catalog-card-top dsc-line-bottom">
-                <img src={pneuImg} alt="Computer" />
-              </div>
-              <div className="dsc-catalog-card-bottom">
-                <h3>R$ 320,00</h3>
-                <h4>
-                  Pneu Continental 185/65 r15
-                </h4>
-              </div>
-            </div>
-            <div className="dsc-card">
-              <div className="dsc-catalog-card-top dsc-line-bottom">
-                <img src={pneuImg} alt="Computer" />
-              </div>
-              <div className="dsc-catalog-card-bottom">
-                <h3>R$ 320,00</h3>
-                <h4>Pneu Continental 185/65 r15</h4>
-              </div>
-            </div>
-            <div className="dsc-card">
-              <div className="dsc-catalog-card-top dsc-line-bottom">
-                <img src={pneuImg} alt="Computer" />
-              </div>
-              <div className="dsc-catalog-card-bottom">
-                <h3>R$ 320,00</h3>
-                <h4>Pneu Continental 185/65 r15</h4>
-              </div>
-            </div>
-            <div className="dsc-card">
-              <div className="dsc-catalog-card-top dsc-line-bottom">
-                <img src={pneuImg} alt="Computer" />
-              </div>
-              <div className="dsc-catalog-card-bottom">
-                <h3>R$ 320,00</h3>
-                <h4>Pneu Continental 185/65 r15</h4>
-              </div>
-            </div>
-            <div className="dsc-card">
-              <div className="dsc-catalog-card-top dsc-line-bottom">
-                <img src={pneuImg} alt="Computer" />
-              </div>
-              <div className="dsc-catalog-card-bottom">
-                <h3>R$ 320,00</h3>
-                <h4>Pneu Continental 185/65 r15</h4>
-              </div>
-            </div>
-            <div className="dsc-card">
-              <div className="dsc-catalog-card-top dsc-line-bottom">
-                <img src={pneuImg} alt="Computer" />
-              </div>
-              <div className="dsc-catalog-card-bottom">
-                <h3>R$ 320,00</h3>
-                <h4>Pneu Continental 185/65 r15</h4>
-              </div>
-            </div>
-            <div className="dsc-card">
-              <div className="dsc-catalog-card-top dsc-line-bottom">
-                <img src={pneuImg} alt="Computer" />
-              </div>
-              <div className="dsc-catalog-card-bottom">
-                <h3>R$ 320,00</h3>
-                <h4>Pneu Continental 185/65 r15</h4>
-              </div>
-            </div>
-            <div className="dsc-card">
-              <div className="dsc-catalog-card-top dsc-line-bottom">
-                <img src={pneuImg} alt="Computer" />
-              </div>
-              <div className="dsc-catalog-card-bottom">
-                <h3>R$ 320,00</h3>
-                <h4>Pneu Continental 185/65 r15</h4>
-              </div>
-            </div>
-            <div className="dsc-card">
-              <div className="dsc-catalog-card-top dsc-line-bottom">
-                <img src={pneuImg} alt="Computer" />
-              </div>
-              <div className="dsc-catalog-card-bottom">
-                <h3>R$ 320,00</h3>
-                <h4>Pneu Continental 185/65 r15</h4>
-              </div>
-            </div>
+            <CatalogCard/>
+            <CatalogCard/>
+            <CatalogCard/>
+            <CatalogCard/>
+            <CatalogCard/>
+            <CatalogCard/>
+            <CatalogCard/>
+            <CatalogCard/>
           </div>
 
           <div className="dsc-btn-next-page">Carregar mais</div>
