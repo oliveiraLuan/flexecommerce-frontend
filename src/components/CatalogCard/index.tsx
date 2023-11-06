1bb57f678fcb528b46e875879e7e7836186ddeeb
Luan de Oliveira <luan012012@hotmail.com>
2023-11-06 18:15:19 -0300

    Criado props para card do catálogo

--- src/components/CatalogCard/index.tsx
+++ src/components/CatalogCard/index.tsx
@@ -1,15 +1,19 @@
-import pneuImg from "../../assets/pneu.png";
+import { ProductDTO } from "../../models/product";
 import './styles.css';
 
-export default function CatalogCard() {
+type Props = {
+  product : ProductDTO;
+};
+
+export default function CatalogCard( {product} : Props) {
   return (
     <div className="dsc-card">
       <div className="dsc-catalog-card-top dsc-line-bottom">
-        <img src={pneuImg} alt="Computer" />
+        <img src={product.imgUrl} alt={product.name} />
       </div>
       <div className="dsc-catalog-card-bottom">
-        <h3>R$ 280,00</h3>
-        <h4>Pneu Continental 185/65 r15</h4>
+        <h3>R$ {product.price.toFixed(2)}</h3>
+        <h4>{product.name}</h4>
       </div>
     </div>
   );
