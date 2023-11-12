09361892cc711cd8c08961b91a0a2be7279fc4c6
Luan de Oliveira <luan012012@hotmail.com>
2023-11-11 21:29:18 -0300

    Criado links para as páginas de cart, login e inicio

--- src/components/HeaderClient/index.tsx
+++ src/components/HeaderClient/index.tsx
@@ -1,20 +1,25 @@
-import cartIcon from '../../assets/cart.svg';
-import './styles.css';
+import { Link } from "react-router-dom";
+import cartIcon from "../../assets/cart.svg";
+import "./styles.css";
 
-export default function HeaderClient(){
-    return (
-        <header className="dsc-header-client">
+export default function HeaderClient() {
+  return (
+    <header className="dsc-header-client">
       <nav className="dsc-container">
-        <h1>Flex Autopeças</h1>
+        <Link to="/">
+          <h1>Flex Autopeças</h1>
+        </Link>
         <div className="dsc-navbar-right">
           <div className="dsc-menu-items-container">
             <div className="dsc-menu-item">
-              <img src={cartIcon} alt="Carrinho de compras" />
+              <Link to="/cart">
+                <img src={cartIcon} alt="Carrinho de compras" />
+              </Link>
             </div>
           </div>
-          <a href="#">Entrar</a>
+          <Link to="/login">Entrar</Link>
         </div>
       </nav>
     </header>
-    );
+  );
 }
--- src/routes/ProductDetails/index.tsx
+++ src/routes/ProductDetails/index.tsx
@@ -2,26 +2,25 @@ import ButtonPrimary from "../../components/ButtonPrimary";
 import ButtonSecondary from "../../components/ButtonSecondary";
 import ProductDetailsCard from "../../components/ProductDetailsCard";
 import "./styles.css";
-import * as productService from '../../services/product-service';
+import * as productService from "../../services/product-service";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function ProductDetails() {
-
   const params = useParams();
   const product = productService.findById(Number(params.productId));
-  
+
   return (
-      <main>
-        <section id="product-details-section" className="dsc-container">
-          {
-            product &&
-            <ProductDetailsCard product={product} />
-          }
-          <div className="dsc-btn-page-container">
-            <ButtonPrimary textButton="Comprar" />
+    <main>
+      <section id="product-details-section" className="dsc-container">
+        {product && <ProductDetailsCard product={product} />}
+        <div className="dsc-btn-page-container">
+          <ButtonPrimary textButton="Comprar" />
+          <Link to="/">
             <ButtonSecondary textButton="Início" />
-          </div>
-        </section>
-      </main>
+          </Link>
+        </div>
+      </section>
+    </main>
   );
 }
