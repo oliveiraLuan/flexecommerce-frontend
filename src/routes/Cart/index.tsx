86d78cf622f5f8b57e0444c9af0e99f2cd94665a
Luan de Oliveira <luan012012@hotmail.com>
2023-12-06 08:58:27 -0300

    Adicionado subTotal e total do carrinho

--- src/routes/Cart/index.tsx
+++ src/routes/Cart/index.tsx
@@ -4,34 +4,45 @@ import { OrderDTO } from "../../models/order";
 import * as cartService from "../../services/cart-service";
 
 export default function Cart() {
-
   const [cart, setCart] = useState<OrderDTO>(cartService.get());
 
   return (
     <>
       <main>
         <section id="cart-container-section" className="dsc-container">
-          <div className="dsc-card dsc-mb20">
-            {cart.items.map((item) => (
-              <div key={item.productId} className="dsc-cart-item-container dsc-line-bottom">
-                <div className="dsc-cart-item-left">
-                  <img src={item.imgUrl} alt={item.name} />
-                  <div className="dsc-cart-item-description">
-                    <h3>{item.name}</h3>
-                    <div className="dsc-cart-item-quantity-container">
-                      <div className="dsc-cart-item-quantity-btn">-</div>
-                      <p>{item.quantity}</p>
-                      <div className="dsc-cart-item-quantity-btn">+</div>
+          {cart.items.length === 0 ? (
+            <div className="dsc-section-title dsc-mb20">
+              <h2>O carrinho está vazio</h2>
+            </div>
+          ) : (
+            <div className="dsc-card dsc-mb20">
+              {cart.items.map((item) => (
+                <div
+                  key={item.productId}
+                  className="dsc-cart-item-container dsc-line-bottom"
+                >
+                  <div className="dsc-cart-item-left">
+                    <img src={item.imgUrl} alt={item.name} />
+                    <div className="dsc-cart-item-description">
+                      <h3>{item.name}</h3>
+                      <div className="dsc-cart-item-quantity-container">
+                        <div className="dsc-cart-item-quantity-btn">-</div>
+                        <p>{item.quantity}</p>
+                        <div className="dsc-cart-item-quantity-btn">+</div>
+                      </div>
                     </div>
                   </div>
+                  <div className="dsc-cart-item-right">
+                    R$ {(item.subTotal).toFixed(2)}
+                  </div>
                 </div>
-                <div className="dsc-cart-item-right">R$ {(item.price * item.quantity).toFixed(2)}</div>
+              ))}
+              <div className="dsc-cart-total-container">
+                <h3>R$ {(cart.total).toFixed(2)}</h3>
               </div>
-            ))}
-            <div className="dsc-cart-total-container">
-              <h3>R$ 640,00</h3>
             </div>
-          </div>
+          )}
+
           <div className="dsc-btn-page-container">
             <div className="dsc-btn dsc-btn-blue">Finalizar pedido</div>
             <div className="dsc-btn dsc-btn-white">Continuar comprando</div>
