daa93b7cf7c3d5292cace61f2c309124b4fdfd76
Luan de Oliveira <luan012012@hotmail.com>
2023-12-22 21:53:11 -0300

    Criado AccessTokenRepository

--- src/utils/system.ts
+++ src/utils/system.ts
@@ -1,5 +1,6 @@
 export const BASE_URL = "http://localhost:8080";
 export const CART_KEY =  "com.flexautopecas.flexecommerce/Cart";
+export const TOKEN_KEY = "com.flexautopecas.flexecommerce/Token";
 
 export const CLIENT_ID = "myclientid";
 export const CLIENT_SECRET = "myclientsecret";
