5964c5139c8b4e232081647dcfa636b1efab27e5
Luan de Oliveira <luan012012@hotmail.com>
2024-01-01 21:58:56 -0300

    Adicionado decisor para cabeçalho e links

--- src/components/HeaderClient/index.tsx
+++ src/components/HeaderClient/index.tsx
@@ -5,6 +5,7 @@ import iconAdmin from "../../assets/admin.svg";
 import { hasAnyRoles } from "../../services/auth-service";
 import { useContext } from "react";
 import { ContextToken } from "../../utils/context-token";
+import LoggedUser from "../LoggedUser";
 export default function HeaderClient() {
 
   const {contextTokenPayload} = useContext(ContextToken);
@@ -32,7 +33,7 @@ export default function HeaderClient() {
               </Link>
             </div>
           </div>
-          <Link to="/login">Entrar</Link>
+          <LoggedUser/>
         </div>
       </nav>
     </header>
