f02314dc471c8963d67b4792c1b73ee7821590b1
Luan de Oliveira <luan012012@hotmail.com>
2024-01-14 23:31:08 -0300

    Adicionado link para o btn de cancelar adição de produto

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -1,45 +1,59 @@
 import { useParams } from "react-router-dom";
 import "./styles.css";
+import ButtonSecondary from "../../../components/ButtonSecondary";
+import { Link } from "react-router-dom";
 
-export default function ProductForm(){
-    
-    const params = useParams();
-    
-    return (
-        <main>
+export default function ProductForm() {
+  const params = useParams();
+
+  return (
+    <main>
       <section id="product-form-section" className="dsc-container">
         <div className="dsc-product-form-container">
           <form className="dsc-card dsc-form">
             <h2>Dados do produto</h2>
             <div className="dsc-form-controls-container">
               <div>
-                <input className="dsc-form-control" type="text" placeholder="Nome"/>
+                <input
+                  className="dsc-form-control"
+                  type="text"
+                  placeholder="Nome"
+                />
               </div>
               <div>
-                <input className="dsc-form-control" type="text" placeholder="Preço"/>
+                <input
+                  className="dsc-form-control"
+                  type="text"
+                  placeholder="Preço"
+                />
               </div>
               <div>
-                <input className="dsc-form-control" type="text" placeholder="Imagem"/>
+                <input
+                  className="dsc-form-control"
+                  type="text"
+                  placeholder="Imagem"
+                />
               </div>
+              <div></div>
               <div>
-                <select className="dsc-form-control dsc-select" required>
-                  <option value="" disabled selected>Categorias</option>
-                  <option value="1">Valor 1</option>
-                  <option value="2">Valor 2</option>
-                </select>
-              </div>
-              <div>
-                <textarea className="dsc-form-control dsc-textarea" placeholder="Descrição"></textarea>
+                <textarea
+                  className="dsc-form-control dsc-textarea"
+                  placeholder="Descrição"
+                ></textarea>
               </div>
             </div>
 
             <div className="dsc-product-form-buttons">
-              <button type="reset" className="dsc-btn dsc-btn-white">Cancelar</button>
-              <button type="submit" className="dsc-btn dsc-btn-blue">Salvar</button>
+              <Link to="/admin/products">
+                <ButtonSecondary textButton="Cancelar" />
+              </Link>
+              <button type="submit" className="dsc-btn dsc-btn-blue">
+                Salvar
+              </button>
             </div>
           </form>
         </div>
       </section>
     </main>
-    );
+  );
 }
