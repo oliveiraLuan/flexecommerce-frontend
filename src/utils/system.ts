76c5a4d79a1b93c0d21fbcc33e255349de9fc7dc
Luan de Oliveira <luan012012@hotmail.com>
2024-01-26 23:45:21 -0300

    Alterado variáveis de ambiente

--- src/utils/system.ts
+++ src/utils/system.ts
@@ -1,6 +1,6 @@
-export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8080";
 export const CART_KEY =  "com.flexautopecas.flexecommerce/Cart";
 export const TOKEN_KEY = "com.flexautopecas.flexecommerce/Token";
 
-export const CLIENT_ID = "myclientid";
-export const CLIENT_SECRET = "myclientsecret";
+export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID ?? "myclientid";
+export const CLIENT_SECRET = import.meta.env.VITE_CLIENT_SECRET ?? "myclientsecret";
